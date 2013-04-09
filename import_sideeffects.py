9948fb4dba8dea8e8c152dbfd5bd74f2ca15118b
Alexander Schrijver <alexander.schrijver@gmail.com>
2013-04-09 12:17:16 +0200

    Sorry for the big diff, i'm too lazy to chop it into smaller pieces.
    This hasn't been tested thoroughly yet, since proper testing takes a long time.
    
    Move all the line parsing/mapping stuff into classes.
    
    Move the line iteration into a single function.
    
    Be more verbose.

--- import_sideeffects.py
+++ import_sideeffects.py
@@ -14,13 +14,150 @@
 # ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
 # OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
+import sys
+
 from django.core.management import setup_environ
-import settings
+from django import db
+
 from drugs.models import *
+import settings
 
 setup_environ(settings)
 
-import sys
+class ParseLine(object):
+    def __init__(self):
+        self.columns = {}
+
+    def __getattr__(self, name):
+        return self.columns[name]
+        
+    def parse(self, line):
+        split_line = line.split("\t")
+
+        for i in range(len(split_line)):
+            self.columns[self.column_names[i]] = split_line[i] 
+
+    def __unicode__():
+        s = ''
+
+        for name, value in self.columns.items():
+            s += "%s: %s, " % (name, value)
+
+        return s
+
+class MeddraFreqParsedLine(ParseLine):
+    column_names = ['flat_compound_id', 'stereo_compound_id', 'source_label',
+        'umls_concept_id', 'concept_name', 'placebo', 'frequency_description',
+        'lower_bound_frequency', 'upper_bound_frequency',
+        'MedDRA_concept_type', 'UMLS_concept_id_for_MedDRA_term',
+        'MedDRA_side_effect_name']
+
+class MeddraAdverseEffectLine(ParseLine):
+    column_names = ['flat_compound_id', 'stereo_compound_id', 'umls_concept_id', \
+        'drug_name', 'side_effect_name', 'MedDRA_concept_type', \
+        'UMLS_concept_id_for_MedDRA_term', 'MedDRA_side_effect_name' ]
+
+def for_every_line(f, cb):
+
+    # Calculate the number of lines. Which will be used to show the progress.
+    line_count = 0
+    while True:
+        line = f.readline()
+
+        # End of file
+        if line == "":
+            break
+
+        line_count += 1
+
+    # Reset read pointer
+    f.seek(0)
+
+    line_number = 1
+    while True:
+        line = f.readline()
+
+        # End of file
+        if line == "":
+            break
+
+        # Skip empty lines
+        if (line.isspace()):
+            continue
+
+        # Trim newlines and such
+        line = line.rstrip();
+
+        print "Processing line: %d of %d %f%%" % (line_number, line_count,
+            float(line_number) / float(line_count) * 100.0)
+
+        cb(line)
+
+        line_number += 1
+
+        # Clear the list of SQL queries Django keeps.
+        db.reset_queries()
+
+def adverse_effect_line(line):
+    effect_line = MeddraAdverseEffectLine()
+    effect_line.parse(line)
+
+    try:
+        side_effect = SideEffect.objects.get(name = effect_line.side_effect_name)
+    except SideEffect.DoesNotExist:
+        side_effect = SideEffect()
+
+    side_effect.name = effect_line.side_effect_name
+    side_effect.meddra_name = effect_line.MedDRA_side_effect_name 
+
+    side_effect.save()
+
+    try:
+        drug = Drug.objects.get(name = effect_line.drug_name)
+    except Drug.DoesNotExist:
+        drug = Drug()
+
+    drug.name = effect_line.drug_name
+    drug.flat_compound_id = effect_line.flat_compound_id
+    drug.stereo_compound_id = effect_line.stereo_compound_id
+
+    drug.save()
+
+    try:
+        side_effect_freq = SideEffectFrequency.objects.get(side_effect = side_effect, drug = drug)
+    except SideEffectFrequency.DoesNotExist:
+        side_effect_freq = SideEffectFrequency(side_effect = side_effect, drug = drug)
+
+    side_effect_freq.save()
+
+def meddra_freq_parsed_line(line):
+    freq_line = MeddraFreqParsedLine()
+    freq_line.parse(line)
+
+    try:
+        side_effect = SideEffect.objects.get(name = freq_line.concept_name)
+    except SideEffect.DoesNotExist:
+        side_effect = SideEffect()
+
+    side_effect.save()
+
+    try:
+        drug = Drug.objects.get(stereo_compound_id = freq_line.stereo_compound_id)
+    except Drug.DoesNotExist:
+        drug = Drug()
+
+    drug.save()
+
+    try:
+        side_effect_freq = SideEffectFrequency.objects.get(side_effect = side_effect, drug = drug)
+    except SideEffectFrequency.DoesNotExist:
+        side_effect_freq = SideEffectFrequency(side_effect = side_effect, drug = drug)
+
+    side_effect_freq.frequency_description = freq_line.frequency_description
+    side_effect_freq.frequency_lower = freq_line.lower_bound_frequency
+    side_effect_freq.frequency_upper = freq_line.upper_bound_frequency
+
+    side_effect_freq.save()
 
 if __name__ == '__main__':
     label_mapping = open('sideeffects_files/label_mapping.tsv', 'r')
@@ -45,82 +182,7 @@ if __name__ == '__main__':
 #
 
     meddra_adverse_effects = open('sideeffects_files/meddra_adverse_effects.tsv', 'r')
+    for_every_line(meddra_adverse_effects, adverse_effect_line) 
 
-    for line in meddra_adverse_effects.readlines():
-
-        # Skip empty lines
-        if (len(line) == 0 or line.isspace()):
-            continue
-
-        flat_compound_id, stereo_compound_id, umls_concept_id, drug_name, \
-            side_effect_name, MedDRA_concept_type, \
-            UMLS_concept_id_for_MedDRA_term, \
-            MedDRA_side_effect_name = line.split('\t')
-
-        try:
-            side_effect = SideEffect.objects.get(name = side_effect_name)
-        except SideEffect.DoesNotExist:
-            side_effect = SideEffect()
-
-        side_effect.name = side_effect_name
-        side_effect.meddra_name = MedDRA_side_effect_name 
-
-        side_effect.save()
-
-        try:
-            drug = Drug.objects.get(name = drug_name)
-        except Drug.DoesNotExist:
-            drug = Drug()
-
-        drug.name = drug_name
-        drug.flat_compound_id = flat_compound_id
-        drug.stereo_compound_id = stereo_compound_id
-
-        drug.save()
-
-        try:
-            side_effect_freq = SideEffectFrequency.objects.get(side_effect = side_effect, drug = drug)
-        except SideEffectFrequency.DoesNotExist:
-            side_effect_freq = SideEffectFrequency(side_effect = side_effect, drug = drug)
-
-        side_effect_freq.save()
-
-    meddra_freq_parsed  = open('sideeffects_files/meddra_freq_parsed.tsv', 'r')
-
-    for line in meddra_freq_parsed.readlines():
-
-        # Skip empty lines
-        if (len(line) == 0 or line.isspace()):
-            continue
-
-        flat_compound_id, stereo_compound_id, source_label, umls_concept_id, concept_name, placebo, \
-            frequency_description, lower_bound_frequency, \
-            upper_bound_frequency, MedDRA_concept_type, \
-            UMLS_concept_id_for_MedDRA_term, \
-            MedDRA_side_effect_name = line.split('\t')
-
-        try:
-            side_effect = SideEffect.objects.get(meddra_name = MedDRA_side_effect_name)
-        except SideEffect.DoesNotExist:
-            side_effect = SideEffect()
-
-        side_effect.save()
-
-        try:
-            drug = Drug.objects.get(stereo_compound_id = stereo_compound_id)
-        except Drug.DoesNotExist:
-            drug = Drug()
-
-        drug.save()
-
-        try:
-            side_effect_freq = SideEffectFrequency.objects.get(side_effect = side_effect, drug = drug)
-        except SideEffectFrequency.DoesNotExist:
-            side_effect_freq = SideEffectFrequency(side_effect = side_effect, drug = drug)
-
-        side_effect_freq.frequency_description = frequency_description
-        side_effect_freq.frequency_lower = lower_bound_frequency
-        side_effect_freq.frequency_upper = upper_bound_frequency
-
-        side_effect_freq.save()
-
+    meddra_freq_parsed = open('sideeffects_files/meddra_freq_parsed.tsv', 'r')
+    for_every_line(meddra_freq_parsed, meddra_freq_parsed_line) 
