e10de9321e1236fab2e4691542b890e235983234
Alexander Schrijver <alexander.schrijver@gmail.com>
2013-04-13 18:03:20 +0200

    Add dirty fields mixin so we can see if fields have been changed or not.

--- drugs/models.py
+++ drugs/models.py
@@ -1,10 +1,39 @@
 from django.db import models
 
-class SideEffect(models.Model):
+from django.db.models.signals import post_save
+
+# Copied from: https://github.com/smn/django-dirtyfields/blob/master/src/dirtyfields/dirtyfields.py
+# Adapted from http://stackoverflow.com/questions/110803/dirty-fields-in-django
+
+class DirtyFieldsMixin(object):
+    def __init__(self, *args, **kwargs):
+        super(DirtyFieldsMixin, self).__init__(*args, **kwargs)
+        post_save.connect(reset_state, sender=self.__class__, 
+                            dispatch_uid='%s-DirtyFieldsMixin-sweeper' % self.__class__.__name__)
+        reset_state(sender=self.__class__, instance=self)
+
+    def _as_dict(self):
+        return dict([(f.name, getattr(self, f.name)) for f in self._meta.local_fields if not f.rel])
+    
+    def get_dirty_fields(self):
+        new_state = self._as_dict()
+        return dict([(key, value) for key, value in self._original_state.iteritems() if value != new_state[key]])
+    
+    def is_dirty(self):
+        # in order to be dirty we need to have been saved at least once, so we
+        # check for a primary key and we need our dirty fields to not be empty
+        if not self.pk: 
+            return True
+        return {} != self.get_dirty_fields()
+
+def reset_state(sender, instance, **kwargs):
+    instance._original_state = instance._as_dict()
+
+class SideEffect(DirtyFieldsMixin, models.Model):
     name = models.CharField(max_length=64, verbose_name='name', db_index=True)
     meddra_name = models.CharField(max_length=64, verbose_name='name')
 
-class Drug(models.Model):
+class Drug(DirtyFieldsMixin, models.Model):
     name = models.CharField(max_length=64, verbose_name='name', db_index=True)
 
     atc_code = models.CharField(max_length=64, verbose_name='ATC code')
@@ -19,7 +48,7 @@ class Drug(models.Model):
 #    generic_name = models.CharField(max_length=64, verbose_name='name')
 #    brand_name = models.CharField(max_length=64, verbose_name='name')
 
-class SideEffectFrequency(models.Model):
+class SideEffectFrequency(DirtyFieldsMixin, models.Model):
     drug = models.ForeignKey(Drug, blank=True, null=True)
     side_effect = models.ForeignKey(SideEffect, blank=True, null=True)
     frequency_description = models.CharField(max_length=64, verbose_name='Frequency description')
